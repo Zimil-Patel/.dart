7c56458c5b1d8360eeb706bfaf71438a152c6f94
Zimil-Patel <patelzimil2310@gmail.com>
2023-12-21 20:41:19 +0530

    21-12-2023

--- DataTypes.dart
+++ DataTypes.dart
@@ -7,6 +7,10 @@ main() {
   var var3 = "Hello world!";
   var var4 = '65';
   var list1 = [1, 2.4, "Hello", ' ', 'W', 'o', 'r', 'l', 'd', '!'];
+  var map1 = {'name ': 'Joseph', 'age ': 25, 'Branch ': 'Computer Science'};
+  var heart = "\u2665";
+  var sameHeart = "\u{2665}";
+  var laugh = '\u{1f600}';
 
   print("\n- - - Data types are not specified - - -\n");
   print(">> Infered by dart compiler : $var1");
@@ -14,6 +18,10 @@ main() {
   print(">> Infered by dart compiler : $var3");
   print(">> Infered by dart compiler : $var4");
   print(">> Infered by dart compiler : $list1");
+  print(">> Infered by dart compiler : $map1");
+  print(">> Infered by dart compiler : $heart");
+  print(">> Infered by dart compiler : $sameHeart");
+  print(">> Infered by dart compiler : $laugh");
 
   print("\n- - - Data types are specified - - -\n");
 
@@ -23,11 +31,14 @@ main() {
   String str = "Hello Star!";
   bool isValid = true;
   List list2 = [2, 4.5, "Hello", ' ', 'W', 'o', 'r', 'l', 'd', '!'];
+  Map map2 = {"Age ": 25, "Name ": "Star BoY", "City ": "Surat"};
+
   print(">> Predefined data type (int) : $num1");
   print(">> Predefined data type (double) : $pi");
   print(">> Predefined data type (String) : $str");
   print(">> Predefined data type (bool) : $isValid");
   print(">> Predefined data type (List) : $list2");
+  print(">> Predefined data type (Map) : $map2");
 
   print("\n");
 }
