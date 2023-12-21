ff0aa8f16f7dd9698c75d8d295c1554eb7cdb54e
Zimil-Patel <patelzimil2310@gmail.com>
2023-12-21 20:32:48 +0530

    21-12-2023

--- DataTypes.dart
+++ DataTypes.dart
@@ -6,12 +6,14 @@ main() {
   var var2 = 50;
   var var3 = "Hello world!";
   var var4 = '65';
+  var list1 = [1, 2.4, "Hello", ' ', 'W', 'o', 'r', 'l', 'd', '!'];
 
   print("\n- - - Data types are not specified - - -\n");
   print(">> Infered by dart compiler : $var1");
   print(">> Infered by dart compiler : $var2");
   print(">> Infered by dart compiler : $var3");
   print(">> Infered by dart compiler : $var4");
+  print(">> Infered by dart compiler : $list1");
 
   print("\n- - - Data types are specified - - -\n");
 
@@ -20,10 +22,12 @@ main() {
   double pi = 3.14;
   String str = "Hello Star!";
   bool isValid = true;
+  List list2 = [2, 4.5, "Hello", ' ', 'W', 'o', 'r', 'l', 'd', '!'];
   print(">> Predefined data type (int) : $num1");
   print(">> Predefined data type (double) : $pi");
   print(">> Predefined data type (String) : $str");
   print(">> Predefined data type (bool) : $isValid");
+  print(">> Predefined data type (List) : $list2");
 
   print("\n");
 }
