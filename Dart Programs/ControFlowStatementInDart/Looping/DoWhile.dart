92e599517bf2e2a3ddf668da5eb78c687612298d
Zimil-Patel <patelzimil2310@gmail.com>
2023-12-24 19:01:10 +0530

    25-12-2023

---
+++ Dart Programs/ControFlowStatementInDart/Looping/DoWhile.dart
@@ -0,0 +1,28 @@
+
+import 'dart:io';
+
+void main() {
+  var c;
+  int choice;
+  do {
+    print("\n1. Print Hello\n2. Print Javatpoint\n3. Exit\n");
+    print("Enter choice");
+    choice = int.parse(stdin.readLineSync()!);
+    switch (choice) {
+      case 1:
+        print("Hello");
+        break;
+      case 2:
+        print("Javatpoint");
+        break;
+      case 3:
+        exit(0);
+        break;
+
+      default:
+        print("please enter valid choice");
+    }
+    print("do you want to enter more?[y/n] : ");
+    c = stdin.readLineSync();
+  } while (c == 'y');
+}
