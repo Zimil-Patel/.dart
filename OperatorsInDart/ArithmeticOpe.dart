cfaf66f0696daaf527839d1b8c86ad76e0869f04
Zimil-Patel <patelzimil2310@gmail.com>
2023-12-24 11:35:00 +0530

    24-12-2023

--- OperatorsInDart/ArithmeticOpe.dart
+++ OperatorsInDart/ArithmeticOpe.dart
@@ -2,14 +2,17 @@
 import 'dart:io';
 
 void main() {
-  var a, b;
-
   print('\nEnter value of a : ');
-  a = int.parse(stdin.readLineSync()!);
+  int? a = int.parse(stdin.readLineSync()!);
 
   print('Enter value of b : ');
-  b = int.parse(stdin.readLineSync()!);
+  int? b = int.parse(stdin.readLineSync()!);
 
-  print("\nValues\na = $a \nb = $b\n");
-  print('\na + b = ${(a + b)}'); //addition
+  print("\nValues\na = $a, b = $b\n");
+  print('a + b = ${(a + b)}'); //addition
+  print('a - b = ${(a - b)}'); //subtraction
+  print('a * b = ${(a * b)}'); //multiplication
+  print('a / b = ${(a / b)}'); //division return float integer both
+  print('a % b = ${(a % b)}'); //modulo
+  print('a ~/ b = ${(a ~/ b)}'); //tilde slash return integer value
 }
