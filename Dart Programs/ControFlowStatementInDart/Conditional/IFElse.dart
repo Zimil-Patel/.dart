1023efadcf0148ba50248c0c4e754a5e54b0a2e0
Zimil-Patel <patelzimil2310@gmail.com>
2023-12-24 17:50:19 +0530

    25-12-2023

---
+++ Dart Programs/ControFlowStatementInDart/Conditional/IFElse.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+
+void main() {
+  var age;
+
+  print("Enter age : ");
+  age = int.parse(stdin.readLineSync()!);
+
+  if (age >= 18) {
+    print("You are eligible for vote!");
+  } else
+    print("You are not eligible!");
+}
