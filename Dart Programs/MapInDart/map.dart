c6732fdad1d4f4b30c34d63c60f47611c58c0e34
Zimil-Patel <patelzimil2310@gmail.com>
2023-12-25 12:25:38 +0530

    26-12-2023

--- Dart Programs/MapInDart/map.dart
+++ Dart Programs/MapInDart/map.dart
@@ -15,4 +15,14 @@ void main() {
   map2['Surname'] = "Boii";
 
   print(map2['Surname']);
+  print(map2.isEmpty);
+  print(map2.isNotEmpty);
+  print(map2.length);
+  print(map2.keys);
+  print(map2.values);
+  print(map2.containsKey('Name'));
+  print(map2.containsKey('Surname'));
+  print(map2.containsValue('Boii'));
+  map2.remove('Surname');
+  print(map2);
 }
