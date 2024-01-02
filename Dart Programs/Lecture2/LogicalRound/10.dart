cbb83e3dbf11f98edcce18f0ce903c80a10568cb
Zimil-Patel <patelzimil2310@gmail.com>
2024-01-02 11:07:02 +0530

    02-01-2024

---
+++ Dart Programs/Lecture2/LogicalRound/10.dart
@@ -0,0 +1,22 @@
+//10.Wap print all prime no into 1 to N.
+
+import 'dart:io';
+
+void main() {
+  var num = 10;
+  var flag = true;
+
+  print("Prime numbers between 1 to $num : ");
+
+  for (int i = 2; i <= num; i++) {
+    flag = true;
+
+    for (int j = 2; j < i; j++) {
+      if (i % j == 0) flag = false;
+    }
+
+    if (flag) print(i);
+  }
+
+  print("");
+}
