2dea9d50e4ea0666d98bd675510dff113d62fff3
Zimil-Patel <patelzimil2310@gmail.com>
2024-01-02 10:59:15 +0530

    02-01-2024

---
+++ Dart Programs/Lecture2/LogicalRound/9.dart
@@ -0,0 +1,21 @@
+//9. Wap to enter a number and check the no is prime or not?
+
+import 'dart:io';
+
+void main() {
+  var num;
+
+  stdout.write("Enter the number : ");
+  num = int.parse(stdin.readLineSync()!);
+
+  var flag = true;
+
+  for (int i = 2; i < num; i++) {
+    if (num % i == 0) flag = false;
+  }
+
+  if (flag)
+    print("\nGiven number($num) is prime\n");
+  else
+    print("\nGiven number($num) is not prime\n");
+}
