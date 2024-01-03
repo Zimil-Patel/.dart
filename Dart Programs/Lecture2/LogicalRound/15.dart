c7d5b0449f179bacc07df1b1bb77057c81b7d39a
Zimil-Patel <patelzimil2310@gmail.com>
2024-01-03 11:33:50 +0530

    03-01-2024

--- Dart Programs/Lecture2/LogicalRound/15.dart
+++ Dart Programs/Lecture2/LogicalRound/15.dart
@@ -4,27 +4,94 @@ import 'dart:io';
 
 void main() {
   List arr = [];
-  var arrSize = 5;
-  var num;
   var choice = 0;
 
   print("> - - - Enter 5 elements of array - - - <");
 
   for (int i = 0; i < 5; i++) {
-    stdout.write("Enter value of arr[$i] : ");
-    num = int.parse(stdin.readLineSync()!);
-    arr.add(num);
+    stdout.write(">> Enter value of arr[$i] : ");
+    arr.add(int.parse(stdin.readLineSync()!));
   }
 
-  printArray(arr);
+  printArrayWithIndex(arr);
 
-  print("1. Insert");
-  print("2. Update");
-  print("3. Delete");
-  print("4. Exit");
-  stdout.write("Enter your choice [1/2/3/4]");
+  while (true) {
+    print("\n\n");
+    print(" - - - - - - - - - - - - -");
+    print("|\t1. Insert\t |");
+    print("|\t2. Update\t |");
+    print("|\t3. Delete\t |");
+    print("|\t4. Exit  \t |");
+    print(" - - - - - - - - - - - - -");
+    stdout.write(">> Enter your choice [1/2/3/4] : ");
+    choice = int.parse(stdin.readLineSync()!);
+
+    switch (choice) {
+      case 1:
+        {
+          arr = insertValue(arr);
+        }
+        break;
+      case 2:
+        {
+          arr = updateValue(arr);
+        }
+        break;
+      case 3:
+        {
+          arr = deleteValue(arr);
+        }
+        break;
+      case 4:
+        {
+          exit(1);
+        }
+      default:
+        {
+          print("> - Enter valid option! - <");
+        }
+    }
+
+    printArrayWithIndex(arr);
+  }
 }
 
-void printArray(var arr) {
-  print("Array : $arr");
+void printArrayWithIndex(List arr) {
+  print("\n\n> - - Array - - <");
+  for (int i = 0; i < arr.length; i++) print("arr[$i] = ${arr[i]}");
+}
+
+//insert element into array
+List insertValue(List arr) {
+  stdout.write("\n>> Enter value to insert : ");
+  arr.add(int.parse(stdin.readLineSync()!));
+  print("> - Array Updated - <");
+  return arr;
+}
+
+//update value  into array
+List updateValue(List arr) {
+  stdout.write("\n>> At which index you want to replace : ");
+  int index = int.parse(stdin.readLineSync()!);
+  if (index < arr.length) {
+    stdout.write("\n>> Enter value to update : ");
+    arr[index] = int.parse(stdin.readLineSync()!);
+    print("> - Array Updated - <");
+  } else {
+    print("> - Invalid array index! - <");
+  }
+  return arr;
+}
+
+//delete value into array
+List deleteValue(List arr) {
+  stdout.write("\n>> At which index you want to delete value : ");
+  int index = int.parse(stdin.readLineSync()!);
+  if (index < arr.length) {
+    arr.removeAt(index);
+    print("> - Array Updated - <");
+  } else {
+    print("> - Invalid array index! - <");
+  }
+  return arr;
 }
