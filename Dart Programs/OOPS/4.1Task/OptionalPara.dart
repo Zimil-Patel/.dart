2cd4d81736cf7efdfcb3934181b4c0a99139b2ec
Zimil-Patel <patelzimil2310@gmail.com>
2024-01-17 20:32:49 +0530

    17-01-2024

--- Dart Programs/OOPS/4.1Task/OptionalPara.dart
+++ Dart Programs/OOPS/4.1Task/OptionalPara.dart
@@ -1,8 +1,6 @@
 //4. Write a Dart Program to create a function which accepts marks of 3 to 5 occupations
 //and returns a percentage of success to gain a new job as a software developer (using optional parameters).
 
-import 'dart:io';
-
 void main() {
   print('\n> - - - Get success percentage - - - <\n');
 
