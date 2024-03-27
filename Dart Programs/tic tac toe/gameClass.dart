136938e57e60811c52ee78398a4a4d4c1950ad24
Zimil-Patel <patelzimil2310@gmail.com>
2024-03-27 13:10:16 +0530

    27-03-2024

--- Dart Programs/tic tac toe/gameClass.dart
+++ Dart Programs/tic tac toe/gameClass.dart
@@ -10,6 +10,7 @@ class TicTacToe {
 
   //o's and x's turn
   bool _oTurn = true;
+  bool _isWon = false;
 
   //moves counter
   int _counter = 0;
@@ -34,15 +35,20 @@ class TicTacToe {
 
   //next move input
   void _nextMove() {
-    //if it o's turn
-    if (_oTurn) {
-      print('\n-- It\'s O\'s Turn --\n');
-      _takeInput();
-    }
-    //if it's x's turn
-    else {
-      print('\n-- It\'s X\'s Turn --\n');
-      _takeInput();
+    if (_counter < 9 && _isWon == false) {
+      //if it o's turn
+      if (_oTurn) {
+        print('\n-- It\'s O\'s Turn --\n');
+        _takeInput();
+      }
+      //if it's x's turn
+      else {
+        print('\n-- It\'s X\'s Turn --\n');
+        _takeInput();
+      }
+    } else {
+      _showWinner();
+      //exit(0);
     }
   }
 
@@ -54,21 +60,90 @@ class TicTacToe {
     int jIndex = int.parse(stdin.readLineSync()!);
 
     bool isEmpty =
-        (_board[iIndex][jIndex] != ' X ' || _board[iIndex][jIndex] != ' O ')
-            ? true
-            : false;
-
-    print('isEmpty : $isEmpty');
+        (_board[iIndex][jIndex] == ' X ' || _board[iIndex][jIndex] == ' O ')
+            ? false
+            : true;
 
     if (isEmpty) {
       _oTurn ? _board[iIndex][jIndex] = ' O ' : _board[iIndex][jIndex] = ' X ';
       _counter++;
       _oTurn = !_oTurn;
-      _showBoard();
+      _isWon = _checkWinner();
+      if (_isWon) {
+        _showBoard();
+      } else {
+        _showBoard();
+      }
     } else {
       print("\n-- Invalid postion! --\n");
-      _showBoard();
       _nextMove();
+    }
+  }
+
+  //check for winner
+  bool _checkWinner() {
+    //row check
+    //row = 1
+    if (_board[0][0] == _board[0][1] &&
+        _board[0][1] == _board[0][2] &&
+        _board[0][0] != ' - ') {
+      return true;
+    }
+    //row = 2
+    else if (_board[1][0] == _board[1][1] &&
+        _board[1][1] == _board[1][2] &&
+        _board[1][0] != ' - ') {
+      return true;
+    }
+    //row = 3
+    else if (_board[2][0] == _board[2][1] &&
+        _board[2][1] == _board[2][2] &&
+        _board[2][0] != ' - ') {
+      return true;
+    }
+    //col = 1
+    else if (_board[0][0] == _board[1][0] &&
+        _board[1][0] == _board[2][0] &&
+        _board[0][0] != ' - ') {
+      return true;
+    }
+    //col = 2
+    else if (_board[0][1] == _board[1][1] &&
+        _board[1][1] == _board[2][1] &&
+        _board[0][1] != ' - ') {
+      return true;
+    }
+    //col = 3
+    else if (_board[0][2] == _board[1][2] &&
+        _board[1][2] == _board[2][2] &&
+        _board[0][2] != ' - ') {
+      return true;
+    }
+    //diagonal
+    else if (_board[0][0] == _board[1][1] &&
+        _board[1][1] == _board[2][2] &&
+        _board[0][0] != ' - ') {
+      return true;
+    }
+    //aniDiagonal
+    else if (_board[0][2] == _board[1][1] &&
+        _board[1][1] == _board[2][0] &&
+        _board[0][2] != ' - ') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  void _showWinner() {
+    if (_isWon) {
+      if (_oTurn) {
+        print('X won');
+      } else {
+        print('O won');
+      }
+    } else {
+      print('OPPS!, It\'s draw!');
     }
   }
 }
