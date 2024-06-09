4e568ef14f12e7016ca45b8fbaaea2045d59026e
Zimil-Patel <patelzimil2310@gmail.com>
2024-06-09 21:02:58 +0530

    09-06-2024

--- Dart Programs/JSON Data handling/USERS/user_main.dart
+++ Dart Programs/JSON Data handling/USERS/user_main.dart
@@ -1,3 +1,29 @@
+import 'json_data.dart';
 import 'user_model.dart';
 
-void main() {}
+void main() {
+  UserModel userModel;
+  for (int i = 0; i < json_list.length; i++) {
+    userModel = UserModel.getFromJson(json_list[i]);
+
+    print('-----------------------------');
+    print('id: ${userModel.id}');
+    print('name: ${userModel.name}');
+    print('username: ${userModel.username}');
+    print('email: ${userModel.email}');
+    print('address:');
+    print('\tstreet: ' + '${userModel.address.street}');
+    print('\tsuite: ' + '${userModel.address.suite}');
+    print('\tcity: ' + '${userModel.address.city}');
+    print('\tzipcode: ' + '${userModel.address.zipcode}');
+    print('\tgeo:');
+    print('\t\tlat: ' + '${userModel.address.geo.lat}');
+    print('\t\tlng: ' + '${userModel.address.geo.lng}');
+    print('phone: ${userModel.phone}');
+    print('website: ${userModel.website}');
+    print('company:');
+    print('\tname: ' + '${userModel.company.name}');
+    print('\tcatchPhrase: ' + '${userModel.company.catchPhrase}');
+    print('\tbs: ' + '${userModel.company.bs}');
+  }
+}
